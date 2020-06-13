2132ca625f84466d69c68760ec1f7798fa934b40
Maya Schrödl <mayaschroedl@users.noreply.github.com>
2020-06-13 16:56:36 +0200

    check other alternative for sclero/podo node

--- 1_phylo_reconstruction/5.2_phypart_alternative_topo_viz.R
+++ 1_phylo_reconstruction/5.2_phypart_alternative_topo_viz.R
@@ -7,11 +7,21 @@ rm(list=ls())
 
 alt_topos = read.table("D:/ONEDRIVE_AU/OneDrive - Aarhus Universitet/Orania_project/1_phylo_reconstruction/5_phypartstopiecharts/2/out.hist.alts.sum.trees", h=T)
 
-pdf("D:/ONEDRIVE_AU/OneDrive - Aarhus Universitet/Orania_project/1_phylo_reconstruction/5_phypartstopiecharts/2/phypart_first_alts.pdf")
+
+# first alternative
+#pdf("D:/ONEDRIVE_AU/OneDrive - Aarhus Universitet/Orania_project/1_phylo_reconstruction/5_phypartstopiecharts/2/phypart_first_alts.pdf")
 for (node in unique(alt_topos$Node_num)){
  selected = alt_topos[alt_topos$Node_num == node,] # select the rows which correspond to the node
   first_alt_topo = as.character(selected[which.max(selected$Num_genetr_supp),]$Topology) # select the topology that is the most represented in the gene trees (first alternative topology)
   plot(read.tree(text=first_alt_topo), main = paste0("First alternative topology for node ", node, ". \n Number of agreeing gene trees: ", max(selected$Num_genetr_supp)))#plot this topology
 }
 
+dev.off()
+
+
+#other alternatives for ps clade crown node
+node=1 #ps clade crown
+selected = alt_topos[alt_topos$Node_num == node,]
+View(selected)
+plot(read.tree(text=as.character(selected$Topology[4]))) #4: one of the other alternatives (not the major one) # you can check with the other topologies, Podococcus is always placed as sister with a few Oranias # but always supported only by one gene tree!
 dev.off()
