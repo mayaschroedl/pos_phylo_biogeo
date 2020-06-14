70dacdb28e868bdf46f90161e4c62cdbe00826a6
Maya Schrödl <mayaschroedl@users.noreply.github.com>
2020-06-14 04:27:28 +0200

    restructured file order

--- bin/fasta2phylip.R
+++
@@ -1,9 +0,0 @@
-install.packages("seqinr")
-install.packages("ape")
-
-library(seqinr)
-library(ape)
-
-setwd("D:/ONEDRIVE_AU/OneDrive - Aarhus Universitet/Orania_project/3_phylogeny_dating/GENOMEDK_Server/paml/or,orscldyp,sclpod_align_supercontig_75/")
-fastaobject<-seqinr::read.fasta("E1007_aligned_gb_head.fasta")
-ape::write.dna(fastaobject, "E1007_aligned_gb_head.phy", nbcol=1,colsep="", colw=1000000)
